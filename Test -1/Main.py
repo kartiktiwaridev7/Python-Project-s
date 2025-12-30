85a0a6e41e314a6a95cdb818b4c209b10325bdf3
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2025-12-30 22:38:10 +0530

    Add files via upload

---
+++ Test -1/Main.py
@@ -0,0 +1,52 @@
+# Question - 1
+# Python program to calculate GST Goods and Service Tax
+
+# print("Here we calculate GST Value")
+
+# Product_price = int(input("Enter the price of the product "))
+
+# GST_percentage = int(input("Enter the  percentage of the GST "))
+
+# GST = (Product_price*GST_percentage)/100
+
+# print("GST value is the " ,GST)
+
+# Question - 2
+# Python program to find the ASCII Value of entered character
+# a = input("Enter a single character or a number for ASCII value ")
+# b = print(ord(a))
+# print("The ASCII value is the ", b)
+
+# Question - 3
+# Python program to generate the following output 5 10 9
+
+# a =int(input("Enter the number 5 , 10 , 9\n"))
+
+# if(a == 5):
+#     print(a)
+
+# elif(a == 10):
+#     print(a)
+
+# elif(a == 9):
+#     print(a)
+
+# else:
+#     print("You enter the wrong value ")
+ 
+# Question - 4
+# Python program to take the temperatures of all 7 days of the week and displays the average temperature of that week
+
+# print("-----------Avg Temp Calculator-------")
+
+# a = print("Enter the day 1 temp\n")
+# b = print("Enter the day 2 temp\n")
+# c = print("Enter the day 3 temp\n")
+# d= print("Enter the day 4 temp\n")
+# e= print("Enter the day 5 temp\n")
+# f= print("Enter the day 6 temp\n")
+# g= print("Enter the day 7 temp\n")
+
+# temp = (a+b+c+d+e+f+g)/7
+
+# print("The Avg temp " , temp)
