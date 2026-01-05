e31021773339ccf24af432e35667523f56a46e2c
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-01-05 22:11:51 +0530

    Add files via upload

--- OOP's/main.py
+++ OOP's/main.py
@@ -10,15 +10,53 @@
 # s2 = car()
 # print(s2.Address)
 
-class Student:
+# class Student:
     
-    def __init__(self,fullname,Marks):
-        self.name = fullname
-        self.marks = Marks
-        print("Adding new student in the database......")
+#     def __init__(self,fullname,Marks):
+#         self.name = fullname
+#         self.marks = Marks
+#         print("Adding new student in the database......")
 
-s1 = Student("Karan" , 98)
-print(s1.name,s1.marks)
+# s1 = Student("Karan" , 98)
+# print(s1.name,s1.marks)
 
-s2  = Student("arjun",88)
-print(s2.name,s2.marks)
+# s2  = Student("arjun",88)
+# print(s2.name,s2.marks)
+
+# class parent:
+#     def __init__(self ,fullName ,Property):
+#         self.name = fullName
+#         self.property = Property
+        
+# s1 = parent()
+
+# class father:
+#     def __init__(self,Name,HowMuchAdd):
+#         self.name = Name
+#         self.property = HowMuchAdd
+
+# s2 = father()
+
+# class children:
+#     def __init__(self,Name,Dream):
+#         self.Name = Name
+#         self.Dream = Dream
+
+# s3 = children()
+
+# class student:
+#     def __init__(self , Name  , Age):
+        
+#         self.name = Name
+#         self.age = Age
+
+# s1 = student("Ayush",17)
+# s2 = student("Yash",15)
+# s3 = student("Simmy",12)
+
+
+# print(s1.name,s1.age)
+# print(s2.name,s2.age)
+# print(s3.name,s3.age)
+
+
